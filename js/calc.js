3865f2676e85f7af47db140c63f3f7070c30ee64
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2019-09-18 11:31:47 +0200

    added calculator to my site

---
+++ js/calc.js
@@ -0,0 +1,51 @@
+function add() {
+    var num1 = document.getElementById("number1").value;
+    console.log(num1)
+      
+    var num2 = document.getElementById("number2").value;
+    console.log(num2)
+    
+    var result = document.getElementById("result");  
+      
+    result.value = parseInt(num1) + parseInt(num2);
+    }
+    
+    
+    function subtract() {
+    var num1 = document.getElementById("number1").value;
+    console.log(num1)
+      
+    var num2 = document.getElementById("number2").value;
+    console.log(num2)
+    
+    var result = document.getElementById("result");  
+      
+    result.value = parseInt(num1) - parseInt(num2);
+    }
+    
+    
+    function multiply() {
+    var num1 = document.getElementById("number1").value;
+    console.log(num1)
+      
+    var num2 = document.getElementById("number2").value;
+    console.log(num2)
+    
+    var result = document.getElementById("result");  
+      
+    result.value = parseInt(num1) * parseInt(num2);
+    }
+    
+    
+    function divide() {
+    var num1 = document.getElementById("number1").value;
+    console.log(num1)
+      
+    var num2 = document.getElementById("number2").value;
+    console.log(num2)
+    
+    var result = document.getElementById("result");  
+      
+    result.value = parseInt(num1) / parseInt(num2);
+    }
+    
